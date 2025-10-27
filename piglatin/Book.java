6d0a51dc0e503f53be3bbec396e4dd7a127da304
SumedhAthreya123 <sa6149@pleasantonusd.net>
2025-10-27 20:55:51 +0000

    starting part done

--- piglatin/Book.java
+++ piglatin/Book.java
@@ -49,7 +49,13 @@ public class Book {
     public void readFromString(String title, String string) {
         // load a book from an input string.
         this.title = title;
-
+        Scanner input = new Scanner(string);
+        while (input.hasNextLine())
+        {
+		// get the next line and add it to text.
+            text.add(input.nextLine());
+        }
+        input.close();
         // TODO: use Scanner to populate the book
         // use: text.add(line) to add a line to the book.
     }
@@ -63,14 +69,33 @@ public class Book {
             URL bookUrl = URI.create(url).toURL();
             // TODO: use Scanner to populate the book
             // Scanner can open a file on a URL like this:
-            // Scanner(bookUrl.openStream())
+            Scanner scan = new Scanner(bookUrl.openStream());
+            while (scan.hasNextLine()) {
+                appendLine(scan.nextLine());
+            }
+            scan.close();
+
             // use: text.add(line) to add a line to the book.
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
     void writeToFile(String name) {
+        PrintStream filePrintStream = null;
+        try {
+            filePrintStream = new PrintStream(name);
+            for (String str : text) {
+                filePrintStream.println(str);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: File not found - " + e.getMessage());
+        } finally {
+            if (filePrintStream != null) {
+                filePrintStream.close();
+            }
+        }
         // TODO: Add code here to write the contents of the book to a file.
         // Must write to file using provided name.
     }
--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -19,24 +19,6 @@ public class PigLatinTranslator {
 
         String result = "";
 
-
-        if (input.isEmpty() || input.isBlank()) {
-            return "";
-        }
-
-        Scanner scanner = new Scanner(input);
-        while (scanner.hasNext()) {
-            String word = scanner.next();
-            String translatedWord = translateWord(word);
-
-            if(!result.isEmpty()) {
-                result += " ";
-            }
-            result += translatedWord;
-        }
-
-        scanner.close();
-        
         // TODO: translate a string input, store in result.
         // The input to this function could be any English string.
         // It may be made up of many words.
@@ -53,16 +35,119 @@ public class PigLatinTranslator {
         // Start here first!
         // This is the first place to work.
          // delete this line
+        if (input.length() == 0) {
+            return "";
+        }
+        // to find any punctuation
+        String punctuation = "";
+        String cleanWord = input;
 
-        return result;
+        // to remove the puncuation
+        while (cleanWord.length() > 0) {
+            char lastChar = cleanWord.charAt(cleanWord.length() - 1);
+            if (!isLetter(lastChar)) {
+                punctuation = lastChar + punctuation;
+                cleanWord = cleanWord.substring(0, cleanWord.length()-1);
+            } else {
+                break;
+            }
+        }
+        
+        if (cleanWord.length() == 0) {
+            return input;
+        }
+        // to remember if first letter was capital or not
+        boolean firstWasCap = isUpperCase(cleanWord.charAt(0));
+        
+
+        String capsPattern = "";
+        for (int i = 0; i < cleanWord.length(); i++) {
+            char c = lowerWord.charAt(i);
+            if (isVowel(c) || c == "-") {
+                break;
+            }
+            consonantCount ++;d
+
+        }
+
+
+        String pigLatinWord = "";
+        if (consonantCount == 0) {
+            pigLatinWord = lowerWord + "ay";
+        } else {
+            String consonants = lowerWord.substring(0, consonantCount);
+            String rest = lowerWord.substring(consonantCount);
+            pigLatinWord = rest + consonants + "ay";
+        }
+
+        String result = "";
+        for (int i = 0; i < pigLatinWord.length(); i++) {
+            char c = pigLatinWord.charAt(i);
+
+            if (i == 0 && firstWasCap) {
+                result = result + toUpperCase(c);
+            } else if (i == 0 && !firstWasCap) {
+                result = result + toLowerCase(c);
+            } else if (i < pigLatinWord.length() - consonantCount - 2) {
+                int originalIndex = i + consonantCount;
+                if (originalIndex < capsPattern.length() && capsPattern.charAt(originalIndex) == 'Y' && originalIndex != 0) {
+                    result = result + toUpperCase(c);
+                } else {
+                    result = result + c;
+                }
+            } else if (i >= pigLatinWord.length() - consonantCount - 2 && i < pigLatinWord.length() - 2) {
+                int originalIndex = i - (pigLatinWord.length() - consonantCount - 2);
+                if (originalIndex < capsPattern.length() && capsPattern.charAt(originalIndex) == 'Y' && originalIndex != 0) {
+                    result = result + toUpperCase(c);
+                } else {
+                    result = result + toLowerCase(c);
+
+                }
+            } else {
+                result = result + c;
+            }
+            return result + punctuation;
+            
+        }
     
-
+    
     // Add additonal private methods here.
     // For example, I had one like this:
     // private static String capitalizeFirstLetter(String input)
+        private static boolean isVowel(char c) {
+            c = toLowerCase(c);
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        }
+
+        private static boolean isLetter(char c) {
+            c = toLowerCase(c);
+            return (c >= 'a' && c <= 'z');
+        }
+        private static boolean isUpperCase(char c) {
+            return (c >= 'A' && c <= 'Z');
+        }
+        private static char toLowerCase() {
+            if (c >= 'A' && c <= 'Z') {
+                return (char)(c + 32);
+            }   
+        }
+        private static String toLowerCase(String s) {
+            String result = "";
+            for (int i = 0; i < s.length(); i++) {
+                result += toLowerCase(s.charAt(i));
+            }
+        return result;
+        }
+        private static char toUpperCase(char c) {
+            if (c >= 'a' && c <= 'z') {
+                return (char)(c - 32);
+            }
+            return c;
+        }
 
 }
 }
+
 
 
 
