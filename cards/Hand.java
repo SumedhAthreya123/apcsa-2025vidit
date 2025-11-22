fcc55b78296be90e4ffdaad9ad0505429955d329
SumedhAthreya123 <sa6149@pleasantonusd.net>
2025-11-22 06:10:07 +0000

    hand.done

--- cards/Hand.java
+++ cards/Hand.java
@@ -23,5 +23,31 @@ public class Hand {
         if (index < 0 || index >= size) {
             return null;
         }
+        return cards[index];
+    }
+    public Card remove(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        Card removed = cards[index];
+
+        for (int i = index; i < size - 1; i++) {
+            cards[i] = cards[i + 1];
+        }
+
+        cards[size - 1] = null;
+        size--;
+
+        return removed;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < size; i++) {
+            sb.append(cards[i]);
+            if (i < size-1) sb.append(" ");
+        }
+        return sb.toString();
     }
 }
