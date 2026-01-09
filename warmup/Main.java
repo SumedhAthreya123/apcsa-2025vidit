327b16fb6dbc9e5681994761c359f6b3b0cf7cb3
SumedhAthreya123 <sa6149@pleasantonusd.net>
2026-01-09 18:23:32 +0000

    done

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,7 +3,23 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        
+        int number = 20;
+        boolean found = false;
 
+
+        while(!found) {
+            found = true;
+
+            for (int i = 1; i <= 20; i++) {
+                if(number % i != 0) {
+                    found = false;
+                    break;
+                }
+            }
+            if (!found) {
+                number+=20;
+            }
+        }
+        System.out.println("Answer: " + number);
     }
 }
