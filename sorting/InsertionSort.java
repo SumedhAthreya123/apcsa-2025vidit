f07c243456b936495c0bbcb68c6cd2bcafe8d152
SumedhAthreya123 <sa6149@pleasantonusd.net>
2026-02-04 03:41:04 +0000

    selection_done

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -3,6 +3,6 @@ package sorting;
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+        
     }
 }
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -3,6 +3,22 @@ package sorting;
 public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+        if (input == null || input.length <= 1) {
+            return;
+        }
+
+        for (int pos = 0; pos < input.length - 1; pos++) {
+            int minPos = pos;
+            for (int scan = pos + 1; scan < input.length; scan++) {
+                if (input[scan] < input[minPos]) {
+                    minPos = scan;
+                }
+            }
+            if (minPos != pos) {
+                int swap = input[pos];
+                input[pos] = input[minPos];
+                input[minPos] = swap;
+            }
+        }
     }
 }
